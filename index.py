83952afcaff01c3ca2596754bd6dd290dc0a8eb1
ashokkillo <ashok.killo@gmail.com>
2023-01-21 02:20:06 +0530

    test for auto-git

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-hello world
+import os
+import subprocess
+
+os.chdir(r"D:\Z_portifolio\killo_portfolio")
+cwd = os.getcwd()
+
+# stats = subprocess.run(["git", "status"])
+add = subprocess.run(["git", "add","."])
+commit = subprocess.run(["git", "commit","-m","test for auto-git"])
+stats = subprocess.run(["git", "status"])
+
+print(add)
